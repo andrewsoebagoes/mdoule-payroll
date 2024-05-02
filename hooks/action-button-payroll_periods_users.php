c9ddf088837718ba0fa486cd867beebacc919c83
andrewsoebagoes <ndrewbagoes@gmail.com>
2024-05-02 14:28:58 +0700

    action button

--- hooks/action-button-payroll_periods_users.php
+++ hooks/action-button-payroll_periods_users.php
@@ -1,3 +1,7 @@
 <?php
 
-return '<a href="'.routeTo('payroll/bayar-gaji',['filter' =>['period_id' => $data->period_id, 'user_id' => $data->user_id]]).'" class="btn btn-sm btn-info"><i class="fas fa-dollar"></i> '.__('payroll.label.pay').'</a> ';
+if($data->status == 'Belum Di Bayar'){
+    return '<a href="'.routeTo('payroll/bayar-gaji',['filter' =>['period_id' => $data->period_id, 'user_id' => $data->user_id]]).'" class="btn btn-sm btn-info"><i class="fas fa-dollar"></i> '.__('payroll.label.pay').'</a> ';
+}else{
+    return null;
+}
